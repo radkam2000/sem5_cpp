0d503c1343b75ed771aa975aec740f3534c14f2f
radkam2000 <radkam2000@protonmail.com>
2022-11-29 15:35:01 +0100

    lab11 started

---
+++ cpp/lab11/zad1/src/Buffer.cpp
@@ -0,0 +1,10 @@
+#include "Buffer.h"
+#include <iostream>
+
+Buffer::Buffer(){
+    std::cout<<"Konstruktor Buffer"<<std::endl;
+}
+
+Buffer::~Buffer(){
+    std::cout<<"Destruktor Buffer"<<std::endl;
+}

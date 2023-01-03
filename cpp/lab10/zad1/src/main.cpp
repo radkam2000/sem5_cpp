9a7299f352d04b26421e8bbd71f388868429859c
radkam2000 <radkam2000@protonmail.com>
2023-01-03 12:31:29 +0100

    Fixed bug in lab10/zad1 where numbers not found was printed even when they were found

--- cpp/lab10/zad1/src/main.cpp
+++ cpp/lab10/zad1/src/main.cpp
@@ -9,11 +9,15 @@ int main(){
     std::cin>>tekst;
     std::smatch res;
     std::cout<<"znalezione liczby: "<<std::endl;
+    bool found=false;
     while(std::regex_search(tekst, res ,std::regex("([+-][0-9]+.[0-9]+)"))){
         std::cout<<res.str()<<std::endl;
         tekst.erase(0,tekst.find(res.str())+res.length());
+        found=true;
     }
-    std::cout<<"Liczby nie zostały znalezione"<<std::endl;
+    if(!found){
+        std::cout<<"Liczby nie zostały znalezione"<<std::endl;
+    }
     return 0;
 }
 
